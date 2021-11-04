c294fc53a87713d8b166292ed2cca79dcde17cf4
natnaelh14 <natnaelh14@github.com>
2021-11-03 22:31:36 -0400

    integrated graphql for the user schema

---
+++ schemas/resolvers.js
@@ -0,0 +1,18 @@
+const { User } = require('../models');
+
+const resolvers = {
+  Query: {
+    // USER RESOLVERS
+    users: async () => {
+      return await User.findAll({});
+    },
+    user: async (parent, { id }) => {
+      const user = await User.findOne({
+        where: { id },
+      });
+      return user;
+    },
+  },
+};
+
+module.exports = resolvers;
--- server.js
+++ server.js
@@ -1,41 +1,46 @@
+require('dotenv').config();
 const express = require('express');
 const session = require('express-session');
 const { ApolloServer } = require('apollo-server-express');
 const { typeDefs, resolvers } = require('./schemas');
-
 const PORT = process.env.PORT || 3001;
 
-// const server = new ApolloServer({
-//   typeDefs,
-//   resolvers,
-// });
+async function startApolloServer() {
+  const server = new ApolloServer({
+    typeDefs,
+    resolvers,
+  });
 
-const app = express();
-app.use(express.urlencoded({ extended: false }));
-app.use(express.json());
+  const app = express();
+  app.use(express.urlencoded({ extended: false }));
+  app.use(express.json());
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+  const sequelize = require('./config/connection');
+  const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const sess = {
-  secret: 'Super secret secret',
-  cookie: {},
-  resave: false,
-  saveUninitialized: true,
-  logging: false,
-  store: new SequelizeStore({
-    db: sequelize,
-  }),
-};
+  const sess = {
+    secret: 'Super secret secret',
+    cookie: {},
+    resave: false,
+    saveUninitialized: true,
+    logging: false,
+    store: new SequelizeStore({
+      db: sequelize,
+    }),
+  };
+  app.use(session(sess));
+  await server.start();
 
-app.use(session(sess));
-// server.applyMiddleware({ app });
+  server.applyMiddleware({ app });
 
-app
-  .listen(PORT, () => {
-    console.log(`App listening on port ${PORT}!`);
-    sequelize.sync({ force: false });
-  })
-  // .then(({ url }) => {
-  //   console.log(`YOUR API IS RUNNING AT: ${url} :)`);
-  // });
+  app.listen(() => {
+    new Promise((resolve) => app.listen(PORT, resolve));
+    console.log(`API server running on port ${PORT}!`);
+    console.log(
+      `🚀 Server ready at http://localhost:${PORT}${server.graphqlPath}`
+    );
+    return { server, app };
+  });
+}
+
+startApolloServer();
