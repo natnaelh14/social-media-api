ba339265fc9433cfbb2d7f3068f48647197e2a67
natnaelh14 <natnaelh14@github.com>
2021-11-04 03:15:38 -0400

    created message schema

--- models/index.js
+++ models/index.js
@@ -2,5 +2,6 @@ const User = require('./user');
 const Post = require('./post');
 const Follow = require('./follow');
 const Comment = require('./comment');
+const Message = require('./message');
 
-module.exports = { User, Post, Follow, Comment };
+module.exports = { User, Post, Follow, Comment, Message };
