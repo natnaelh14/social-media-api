9e730d389c87e535956c801735688a0c9d97da8f
natnaelh14 <natnaelh14@github.com>
2021-11-04 03:26:29 -0400

    created message seed data

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,11 +1,12 @@
 const sequelize = require('../config/connection');
-const { User, Post, Follow, Comment } = require('../models');
+const { User, Post, Follow, Comment, Message } = require('../models');
 const userData = require('./userData.json');
 const postData = require('./postData.json');
 const followData = require('./followData.json');
 const commentData = require('./commentData.json');
+const messageData = require('./messageData.json');
 
-const colors = require ('colors');
+const colors = require('colors');
 
 const seedDatabase = async () => {
   try {
@@ -17,19 +18,24 @@ const seedDatabase = async () => {
     });
     //POST
     await Post.bulkCreate(postData, {
-        individualHooks: true,
-        returning: true,
-    })
+      individualHooks: true,
+      returning: true,
+    });
     //FOLLOW
     await Follow.bulkCreate(followData, {
       individualHooks: true,
       returning: true,
-  })
-  //COMMENT
-  await Comment.bulkCreate(commentData, {
-    individualHooks: true,
-    returning: true,
-  })
+    });
+    //COMMENT
+    await Comment.bulkCreate(commentData, {
+      individualHooks: true,
+      returning: true,
+    });
+    //MESSAGE
+    await Message.bulkCreate(messageData, {
+      individualHooks: true,
+      returning: true,
+    });
     console.log('Data Imported!'.green.inverse);
     process.exit(0);
   } catch (e) {
